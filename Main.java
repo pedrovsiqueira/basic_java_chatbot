24cdefe1dc486d6b6f3ee39e94e20b9954ca789f
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-11-14 12:21:40 -0300

    feat: add scanner to get user name

--- Main.java
+++ Main.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String botName = "Aid";
         int birthYear = 2020;
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+
+        String yourName = scanner.next();
+
+        System.out.println("What a great name you have, " + yourName);
+
     }
 }
