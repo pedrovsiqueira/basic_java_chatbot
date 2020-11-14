bc6ff1da77be9c94b03ff4d486611edeb16a4940
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-11-14 10:21:46 -0300

    feat: create botname and birthYear

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String botName = "Aid";
+        int birthYear = 2020;
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+    }
+}
